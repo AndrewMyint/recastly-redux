608fda0e3a2e3d1afdb1135f073af5dd0b9f3073
vvek87 <vvek87@gmal.com>
2019-01-15 20:39:19 -0600

    Complete defining action creators

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,32 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
-
   //TODO:  Write an asynchronous action to handle a video search!
+
+  var callback = function(data) {
+    return data;
+  };
+
+  return function() {
+    return {
+      type: 'searchVideo',
+      videos: searchYouTube({YOUTUBE_API_KEY, q}, callback)
+    };
+  };
 };
 
 export default handleVideoSearch;
+
+
+/*
+
+function not_a_thunk() {
+  // this one is a "thunk" because it defers work for later:
+  return function() {
+    {type: X,
+     videos: searchYouTube({YOUTUBE_API_KEY, q, max}, callback)
+    }
+  };
+}
+\
+*/
